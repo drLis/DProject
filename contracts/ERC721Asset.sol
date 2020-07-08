97fd35821401c656d66c348b4fd94d98ee57be20
dr.lis <dr.lis@outlook.com>
2020-07-08 20:21:05 +0300

    Dimond struct added

--- contracts/ERC721Asset.sol
+++ contracts/ERC721Asset.sol
@@ -4,8 +4,41 @@ import "./_lib/openzeppelin-contracts/contracts/presets/ERC721PresetMinterPauser
 
 contract ERC721Asset is ERC721PresetMinterPauserAutoId
 {
+	struct Diamond
+	{
+		string cut;
+		string clarity;
+		string color;
+		string carat;
+		string system;
+	}
+
 	constructor(string memory name, string memory symbol, string memory baseURI) public ERC721PresetMinterPauserAutoId(name, symbol, baseURI)
 	{
 
 	}
+
+	function mint
+		(address to, string memory cut,
+			string memory clarity, string memory color,
+			string memory carat, string memory system,
+			string memory tokenURI
+		) external
+	{
+		this.mint(to);
+	}
+
+	function getDiamondInfoByTokenId(uint tokenId) public returns
+		(string memory cut, string memory clarity,
+		string memory carat, string memory system,
+		string memory color)
+	{
+		diamonds[tokenId].cut     = cut;
+		diamonds[tokenId].clarity = clarity;
+		diamonds[tokenId].carat   = carat;
+		diamonds[tokenId].system  = system;
+		diamonds[tokenId].color   = color;
+	}
+
+	mapping (uint => Diamond) public diamonds;
 }
